a5197e990b278541e1b0a3bc8cf814f3f4c25677
Joao Victor <joao.dvlp@gmail.com>
2025-05-29 07:39:43 -0300

    feat/set-authorities: implementa authorities nas rotas nescessárias

--- src/main/java/com/library_a3/library_a3/Controllers/AuthController.java
+++ src/main/java/com/library_a3/library_a3/Controllers/AuthController.java
@@ -1,7 +1,7 @@
 package com.library_a3.library_a3.Controllers;
 
 import com.library_a3.library_a3.domains.Credentials;
-import com.library_a3.library_a3.domains.CredentialsRepository;
+import com.library_a3.library_a3.repositories.CredentialsRepository;
 import com.library_a3.library_a3.services.LoginService;
 import com.library_a3.library_a3.shared.CreateCredentialsDTO;
 import com.library_a3.library_a3.shared.dtos.LoginResponseDTO;
