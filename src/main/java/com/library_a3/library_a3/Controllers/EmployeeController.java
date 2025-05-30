1d976fa20a945b14d2e511a176c889583a61bcc8
llercio <lercioluiz1995@gmail.com>
2025-05-30 11:12:11 -0300

    Função para criar novos employees

--- src/main/java/com/library_a3/library_a3/Controllers/EmployeeController.java
+++ src/main/java/com/library_a3/library_a3/Controllers/EmployeeController.java
@@ -1,20 +1,28 @@
 package com.library_a3.library_a3.Controllers;
 
 import com.library_a3.library_a3.domains.Employee;
-import com.library_a3.library_a3.repositories.RoleRepository;
+import com.library_a3.library_a3.repositories.EmployeeRepository;
+import com.library_a3.library_a3.shared.dtos.CreateEmployeeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/employees")
 
-public class RoleController {
+public class EmployeeController {
     @Autowired
-    private RoleRepository roleRepository;
+    private EmployeeRepository employeeRepository;
+
+    @PostMapping()
+    public Employee create(@RequestBody CreateEmployeeDTO body) {
+        Employee employee = new Employee(body.getName(),body.getCategory(), "qualquervalor");
+        return this.employeeRepository.save(employee);
+    }
 
     @GetMapping()
     public List<Employee> getEmployees() {
-        return this.roleRepository.findAll();
+        return this.employeeRepository.findAll();
     }
 }
--- src/main/java/com/library_a3/library_a3/repositories/EmployeeRepository.java
+++ src/main/java/com/library_a3/library_a3/repositories/EmployeeRepository.java
@@ -3,7 +3,5 @@ package com.library_a3.library_a3.repositories;
 import com.library_a3.library_a3.domains.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Employee, String> {
-    Employee findByName(String name);
-    Employee findByRole(String role);
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
 }
