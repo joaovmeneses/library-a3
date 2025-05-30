d05fd38bc9b83ecd132e662bcf4cae5d9baee264
Joao Victor <joao.dvlp@gmail.com>
2025-05-29 21:13:57 -0300

    feat: implemmenta entidade employee

---
+++ src/main/java/com/library_a3/library_a3/domains/Employee.java
@@ -0,0 +1,40 @@
+package com.library_a3.library_a3.domains;
+
+import com.library_a3.library_a3.shared.enums.employees.EmployeeCategoryEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    @Column
+    String name;
+    @Column
+    EmployeeCategoryEnum category;
+    @Column(name = "credential_id")
+    String credentialId;
+    @Column(name = "created_at")
+    private Date createdAt;
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    @Column(name = "deleted_at")
+    private Date deletedAt;
+
+    public Employee (String name, EmployeeCategoryEnum category, String credentialId) {
+        this.category = category;
+        this.name = name;
+        this.credentialId = credentialId;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+}
