24b5cf75c32d721611202739994e53d94a0d6999
Rafael <oliver.rafadev@gmail.com>
2025-06-24 11:28:56 -0300

    Implementa o filtro da organization ao buscar funcionarios

--- src/main/java/com/library_a3/library_a3/Controllers/EmployeeController.java
+++ src/main/java/com/library_a3/library_a3/Controllers/EmployeeController.java
@@ -1,14 +1,23 @@
 package com.library_a3.library_a3.Controllers;
 
-import com.library_a3.library_a3.domains.Employee;
-import com.library_a3.library_a3.repositories.EmployeeRepository;
-import com.library_a3.library_a3.services.employees.CreateEmployeeService;
-import com.library_a3.library_a3.shared.dtos.CreateEmployeeDTO;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.library_a3.library_a3.domains.Employee;
+import com.library_a3.library_a3.repositories.EmployeeRepository;
+import com.library_a3.library_a3.services.TokenService;
+import com.library_a3.library_a3.services.employees.CreateEmployeeService;
+import com.library_a3.library_a3.shared.dtos.CreateEmployeeDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -19,14 +28,21 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
     @Autowired
     private CreateEmployeeService createEmployeeService;
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private HttpServletRequest request;
 
     @PostMapping()
     public Employee create(@Validated @RequestBody CreateEmployeeDTO body) {
+        String token = request.getHeader("Authorization");
+        String organizationId = tokenService.getOrganizationId(token.replace("Bearer ", " ")).asString();
         return this.createEmployeeService.execute(body);
     }
 
     @GetMapping()
     public List<Employee> getEmployees() {
-        return this.employeeRepository.findAll();
+        String token = request.getHeader("Authorization");
+        return this.employeeRepository.getAllEmployeesOrganization(tokenService.getOrganizationId(token.replace("Bearer ", "")).asString());
     }
 }
--- src/main/java/com/library_a3/library_a3/repositories/EmployeeRepository.java
+++ src/main/java/com/library_a3/library_a3/repositories/EmployeeRepository.java
@@ -1,10 +1,16 @@
 package com.library_a3.library_a3.repositories;
 
-import com.library_a3.library_a3.domains.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.library_a3.library_a3.domains.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
     Optional<Employee> getByCredentialId (String credentialId);
+
+    @Query(value = "SELECT * FROM employee e WHERE e.organization_id = :organizationId AND e.deleted_at IS NULL", nativeQuery= true)
+    List<Employee> getAllEmployeesOrganization(String organizationId);
 }
